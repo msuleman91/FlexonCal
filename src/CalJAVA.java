8c11d4901b00724ef265cf48a1e5191609cc6a47
msuleman91 <msuleman387@gmail.com>
2019-05-08 13:12:49 -0700

    my first commit in master

---
+++ src/CalJAVA.java
@@ -0,0 +1,21 @@
+
+
+public class CalJAVA {
+	
+	public int sum(int a, int b)
+	{
+		
+		return a+b;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		CalJAVA cal=new CalJAVA();
+		
+		System.out.println(cal.sum(10, 10));
+		
+
+	}
+
+}
